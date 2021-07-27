0a95f91c5636455d73a8a6c7d2ff365bc0768d8f
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2021-07-27 22:33:57 +0200

    rounded off total values to 2 decimal places

--- settings-bill.js
+++ settings-bill.js
@@ -60,7 +60,6 @@ module.exports = function SettingsBill() {
                 filteredActions.push(action);
             }
 
-            console.log('M')
         }
 
         return filteredActions;
@@ -94,12 +93,12 @@ module.exports = function SettingsBill() {
     }
 
     function totals() {
-        let smsTotal = getTotal('sms')
-        let callTotal = getTotal('call')
+        let smsTotal = getTotal('sms').toFixed(2)
+        let callTotal = getTotal('call').toFixed(2)
         return {
             smsTotal,
             callTotal,
-            grandTotal : grandTotal()
+            grandTotal : grandTotal().toFixed(2)
         }
     }
 
